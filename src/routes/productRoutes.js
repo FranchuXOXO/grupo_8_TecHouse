37958cb498d75d562c4efcf92d30062436673f9a
Francisco Salanova <fsalanova.16@gmail.com>
2022-02-16 22:03:59 -0300

    Correccion en ruta POST de productos

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -5,10 +5,7 @@ const productController = require("../controllers/productController");
 router.get("/Detalle", productController.detailMethod);
 router.get("/carrito", productController.cartMethod);
 router.get("/productos", productController.stockMethod);
-<<<<<<< HEAD
 router.post("/productos", productController.createProduct)
-=======
 router.get("/list", productController.listMethod);
->>>>>>> bb06e84e540ecff67366c8b1b41bd15ec437f02a
 
 module.exports = router;
