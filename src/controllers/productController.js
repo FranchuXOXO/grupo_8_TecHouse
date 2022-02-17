90b1d2eb627552812e79c54a8c7747438ed92ca7
Francisco Salanova <fsalanova.16@gmail.com>
2022-02-16 21:57:35 -0300

    Agregado de metodo para crear productos

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -16,6 +16,19 @@ const productCont={
         res.render("products/productCreate", {
             siteTitle: "Manejo de Stock"
         });
+    },
+
+    createProduct: (req, res) => {
+        let producto = {
+            nombre: req.body.nombre,
+            descripcion: req.body.descripcion,
+            color: req.body.color,
+            categoria: req.body.categoria,
+            precio: req.body.precio
+        }
+
+        return res.send(producto);
     }
 }
-module.exports=productCont
+
+module.exports = productCont;
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const productController = require("../controllers/productController")
+const productController = require("../controllers/productController");
 
-router.get("/Detalle", productController.detailMethod)
-router.get("/carrito", productController.cartMethod)
+router.get("/Detalle", productController.detailMethod);
+router.get("/carrito", productController.cartMethod);
 router.get("/productos", productController.stockMethod);
+router.post("/productos", productController.createProduct)
 
-module.exports=router
+module.exports = router;
