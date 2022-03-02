f79c68611af09b28ca4f2c7d38757b7c530cedf9
Marcelo Sturmann <marcelosturmann@Air-de-Marcelo.fibertel.com.ar>
2022-03-02 19:52:10 -0300

    Se modifico el form de creacion de usuarios y se agrego rutas y controlador

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+const usersFilePath = path.join(__dirname, '../data/users.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+
 const logReg = {
     logMethod: (req, res) => {
         res.render("users/login", {
@@ -9,6 +15,18 @@ const logReg = {
         res.render("users/signup", {
             siteTitle: "Signup",
         });
+    },
+
+    createMethod: (req, res) => {
+        const created = req.body
+        lastId = users[users.length - 1].id + 1;
+        created.id = lastId
+        users.push(created)
+        created.image = req.file.filename
+        fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2))
+        
+        res.send (users)
     }
 }
+
 module.exports=logReg
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,7 +2,22 @@ const express = require('express');
 const router = express.Router();
 const userCont = require("../controllers/userController")
 
+const multer = require('multer');
+const path = require("path")
+const storage = multer.diskStorage({
+ destination: function (req, file, cb) {
+ cb(null, './public/images/users')
+ },
+ filename: function (req, file, cb) {
+ cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname) )
+ }
+})
+const upload = multer({ storage: storage })
+
+
 router.get("/Login", userCont.logMethod)
 router.get('/Signup', userCont.regMethod)
+router.post('/Signup', upload.single("image"), userCont.createMethod)
+
 
 module.exports=router
