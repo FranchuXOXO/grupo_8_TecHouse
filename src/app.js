cd2231f3aa13cc1087600558872c674677c50beb
Francisco Salanova <fsalanova.16@gmail.com>
2022-03-13 16:11:59 -0300

    Revision de codigo y ajustes menores de formato

--- src/app.js
+++ src/app.js
@@ -2,21 +2,21 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+
 const publicPath = path.resolve(__dirname, '../public');
-const rutasIndex = require("./routes/indexRoutes")
-const rutasProduct = require("./routes/productRoutes") 
-const rutasUser = require("./routes/userRoutes")
+const rutasIndex = require("./routes/indexRoutes");
+const rutasProduct = require("./routes/productRoutes"); 
+const rutasUser = require("./routes/userRoutes");
 
 const methodOverride = require('method-override');
-app.use(methodOverride('_method'));
 
+app.use(express.static(publicPath));
+app.use(methodOverride('_method'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
-
-app.use(express.static(publicPath));
 
 app.listen(3000, () => {
     console.log("Servidor corriendo en el puerto 3000");
