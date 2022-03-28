b13dda7c1f16249e7a9c42358d33920dc372b970
Marcelo Sturmann <marcelosturmann@Air-de-Marcelo.fibertel.com.ar>
2022-03-28 19:59:31 -0300

    Sequelize y mysql2 iniciados, y creados los archivos modelos, y configurado el modelo de Color

---
+++ db/models/Color.js
@@ -0,0 +1,22 @@
+const sequelize = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    const alias = 'Color';
+
+
+    const config = {
+        timestamps: false,
+        tableName: 'colors'
+    }
+
+    const columns = {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+        },
+        color: DataTypes.STRING,
+    }
+
+    const Color = sequelize.define(alias, columns, config);
+    return Color
+};
