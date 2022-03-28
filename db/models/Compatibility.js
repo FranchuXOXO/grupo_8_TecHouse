e566873fe238bccfffbe1d42187d3864134224d6
Anamariasalcedoa <96092250+AnaMariasa@users.noreply.github.com>
2022-03-28 20:14:52 -0300

    Creado el modelo de Compatibility

---
+++ db/models/Compatibility.js
@@ -0,0 +1,22 @@
+const sequelize = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    const alias = 'Compatibility';
+
+
+    const config = {
+        timestamps: false,
+        tableName: 'compatibility'
+    }
+
+    const columns = {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+        },
+        device_name: DataTypes.STRING
+    }
+
+    const Compatibility = sequelize.define(alias, columns, config);
+    return Compatibility
+};
