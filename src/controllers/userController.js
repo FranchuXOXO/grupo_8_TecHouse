e5d0bcfd92de19b26d1b435ba355b05ac39dd4e2
Marcelo Sturmann <marcelosturmann@MacBook-Air-de-Marcelo.local>
2022-04-20 20:48:19 -0300

    Trabajando en el login a traves de la base de datos

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -32,69 +32,57 @@ const logReg = {
             return res.send('usuario creado')
         })
             .catch(error => res.send(error))
-        
-        
-        
-        
-        
-        /*
-        const created = req.body
-        lastId = users[users.length - 1].id + 1;
-        created.id = lastId
-        users.push(created)
-        created.image = req.file.filename
-        created.password = bcryptjs.hashSync(req.body.password, 10);
-        fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2))
-
-        res.send(users) //Para arreglar */
+   
     },
 
     loginMethod: (req, res) => {
         let errors = validationResult(req);
         if (errors.isEmpty()) {
             console.log("Valide si errores");
-            const userLogCheck = users.find((user) => user.email == req.body.email);
-            if (userLogCheck) {
-                let isOkThePassword = bcryptjs.compareSync(req.body.password, userLogCheck.password);
-                if (isOkThePassword) {
-                    delete userLogCheck.password;
-                    // Probamos session y cookies
-                    req.session.userLogged = userLogCheck;
-                    console.log(userLogCheck);
+            db.Client.findOne({ where: { email: req.body.email } }).then((userLogCheck) => {
 
-                    if (req.body.remember_user) {
-                        res.cookie('userEmail', req.body.email, { maxAge: (1000 * 60) * 60 })
-                    }
-                    // Hasta acá es la prueba
-                    //return res.render('users/profile', { user: req.session.userLogged, siteTitle: "Perfil" });
-                    logReg.profile(req, res)
+                if (userLogCheck) {
+                    let isOkThePassword = bcryptjs.compareSync(req.body.password, userLogCheck.password);
+                    if (isOkThePassword) {
+                        delete userLogCheck.password;
+                        // Probamos session y cookies
+                        req.session.userLogged = userLogCheck;
+    
+                        if (req.body.remember_user) {
+                            res.cookie('userEmail', req.body.email, { maxAge: (1000 * 60) * 60 })
+                        }
+                        // Hasta acá es la prueba
+                        //return res.render('users/profile', { user: req.session.userLogged, siteTitle: "Perfil" });
+                        logReg.profile(req, res)
+                        }
+                    return res.render('users/login', {
+                        errors: {
+                            email: {
+                                msg: 'La contraseña es invalida'
+                            }
+                        }, siteTitle: "Login",
+                        user: req.session.userLogged
+                        }
+                    );
                 }
                 return res.render('users/login', {
                     errors: {
                         email: {
-                            msg: 'La contraseña es invalida'
+                            msg: 'El usuario es inválido'
                         }
                     }, siteTitle: "Login",
                     user: req.session.userLogged
-                }
-                );
-            }
-            return res.render('users/login', {
-                errors: {
-                    email: {
-                        msg: 'El usuario es inválido'
                     }
-                }, siteTitle: "Login",
-                user: req.session.userLogged
-            }
-            )
+                )
+             })
+             .catch(error => res.send(error));  
         } else {
             res.render('users/login', { errors: errors.mapped(), old: req.body, siteTitle: "Login", user: req.session.userLogged });
         }
     },
     profile: (req, res) => {
        const user = req.session.userLogged;
-	 res.render('users/profile', {
+	    res.render('users/profile', {
 			user,
             siteTitle: "Perfil",
             user: req.session.userLogged
