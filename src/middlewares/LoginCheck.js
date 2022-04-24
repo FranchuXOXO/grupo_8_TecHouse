f73c8c6b72e03e7d422ec77f2d86e7b21782c5bf
mjmadrussan <madru92@gmail.com>
2022-04-24 17:17:08 -0300

    Solucionado error de edicion de producto

--- src/middlewares/LoginCheck.js
+++ src/middlewares/LoginCheck.js
@@ -1,5 +1,5 @@
 function adminMiddleware(req, res, next) {
-	if ( req.session.userLogged.category != "admin") {
+	if ( req.session.userLogged.id_category != "1") {
         res.send("No sos administrador")	
 	}
 	else{
