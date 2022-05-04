1aa8ba6d3ec9ca80e02516fc718aca05302addc8
Francisco Salanova <fsalanova.16@gmail.com>
2022-05-04 10:02:57 -0300

    Modificando formateo y agregado de comentarios en Middlewares

--- src/middlewares/Authenticator.js
+++ src/middlewares/Authenticator.js
@@ -1,7 +1,13 @@
+/*
+	Authenticator (AuthLog):
+	Middleware generado para validar si el usuario que ingresó a la página se encuentra logueado.
+*/
+
 function authMiddleware(req, res, next) {
 	if (!req.session.userLogged) {
-		return res.redirect("/Login");
+		return res.redirect("/Login");	// en caso de no detectar un usuario logueado en la sesión, se redirige al usuario a la pantalla de login.
 	}
+	
 	next();
 }
 
