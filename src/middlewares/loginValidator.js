0fec9cc6e2edd3766c64cc6438ee75e28eb12ccd
Francisco Salanova <fsalanova.16@gmail.com>
2022-05-08 18:20:45 -0300

    Correccion en validator

--- src/middlewares/loginValidator.js
+++ src/middlewares/loginValidator.js
@@ -7,8 +7,9 @@ const { check } = require('express-validator');
 
 module.exports = [
     check('email')
+        .notEmpty().withMessage('Debes completar el email').bail()
         .isEmail().withMessage('Debes ingresar un email válido'),
     check('password')
-        // .notEmpty().withMessage('Debes completar la contraseña').bail()
+        .notEmpty().withMessage('Debes completar la contraseña').bail()
         .isLength({ min: 5 }).withMessage('La contraseña debe tener al menos 5 caracteres')
 ]
